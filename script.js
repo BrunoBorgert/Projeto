e46ca5886aba8e5df0c3c04b561106bce8c80e3f
BrunoBorgert <brunoguilhermeborgert@outlook.com.br>
2023-02-12 19:01:49 -0300

    connection

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-var add = document.getElementById('novo').addEventListener('click', adicionar())
+var mysql = require('mysql');
 
-function adicionar(){
-    alert('Add')
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "91416602bR@"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+function logar(){
+    con.query("select email, senha from usuario where email = " + document.getElementById('user') + " and senha = " + document.getElementById('pass'), function (err, result, fields) {
+        if (err) alert(err);
+        console.log(result);
+        });
+
 }
